53ba0edf60e12d21095077a84003a45b0898dc82
SociaJL <socialconsequences@gmail.com>
2020-10-07 19:42:29 +0000

    updated to add shadow

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $("#text-box").on("keyup", function () {
+  $(".formtext_area").on("keyup", function () {
     let count = 140 - $(this).val().length
     $(".counter").html(count)
     if (count < 0) {
